63e18a4180a8b5343a13a7d32a62cf96add5e396
feruricar <feruricar>
2022-10-11 16:48:56 -0300

    commit final

---
+++ src/infra/sqlalchemy/models/models.py
@@ -0,0 +1,15 @@
+
+
+from email.mime import base
+from sqlalchemy import Column, Integer, String, Float, Boolean
+from src.infra.sqlalchemy.config.database import Base
+
+class Produto(Base):
+
+    __tablename__= 'Produto'
+
+    id = Column(Integer, primary_key=True, index=True) 
+    nome = Column(String)
+    detalhes = Column(String)
+    preco = Column(Float)
+    disponivel = Column(Boolean)
